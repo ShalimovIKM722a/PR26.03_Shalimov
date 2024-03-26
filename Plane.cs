611319e0647e114bed9eccdfb1d9233dde12081b
alexs <alexshalimov2019@gmail.com>
2024-03-26 17:09:08 +0200

    Add project files.

---
+++ Plane.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Channels;
+
+
+namespace PR26._03_Shalimov
+{
+    public class Plane
+    {
+        private  string _brand;
+        private string _model;
+        private string _country;
+        private int _year;
+        private int _fuelQuantity;
+
+        public Plane(string brand, int year, string model, string country, int fuelQuantity )
+        {
+            _brand = brand;
+            _year = year;
+            _model = model;
+            _country = country;
+            _fuelQuantity = fuelQuantity;
+        }
+
+        private  bool Charging()
+        {
+            if (_fuelQuantity > 60000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Flight()
+        {
+            if( Charging() )
+            {
+                Console.WriteLine("Ми можемо взлетіти!");
+            }
+            else
+            {
+                Console.WriteLine("Недостатньо топлива:(");
+            }
+        }
+
+        public void Info() {
+            Console.WriteLine($"Літак марки{_brand}, під назвою {_model} було випущено у {_year} році в {_country}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using PR26._03_Shalimov;
+
+var Mriia = new Plane("KiAPO",1991, "Mriia/AN-225", "Ukraine", 70000);
+Mriia.Flight();
+Mriia.Info();   
+
